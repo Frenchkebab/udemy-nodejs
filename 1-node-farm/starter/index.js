e421aa2bef4f2d33c01eaaed02d6e2560a45cf44
frenchkebab <tubowangin@gmail.com>
2022-01-06 20:17:23 +0900

    [#2] make my first module - replaceTemplate

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const replaceTemplate = require('./modules/replaceTemplate');
 /////////////////////////////////
 // ! FILES
 
@@ -32,18 +33,6 @@ console.log('Will read file!');
 // ! SERVER
 
 // * code out here is only executed 'once' when we run the program
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName); // regular expression : replace all {%PRODUCTNAME%}
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organice');
-  return output;
-};
 
 // stored this to variable 'data' so you don't have to read it each time you redirect '/api'
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
