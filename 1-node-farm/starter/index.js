9cd79ca4c993f63c6c334a27214abf15036dacb8
frenchkebab <tubowangin@gmail.com>
2022-01-06 14:27:37 +0900

    [#2] Building a very simple API

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 /////////////////////////////////
 // ! FILES
 
@@ -28,9 +29,37 @@ const http = require('http');
 console.log('Will read file!');
 
 /////////////////////////////
-// SERVER
+// ! SERVER
+
+// * code out here is only executed 'once' when we run the program
+
+// stored this to variable 'data' so you don't have to read it each time you redirect '/api'
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+// * code below gets executed each time when there is new request
 const server = http.createServer((req, res) => {
-  res.end('Hello from the server!');
+  // console.log(req.url);
+  const pathName = req.url;
+
+  if (pathName === '/' || pathName === '/overview') {
+    res.end('This is the OVERVIEW');
+  } else if (pathName === '/product') {
+    res.end('This is the PRODUCT!');
+  } else if (pathName === '/api') {
+    // fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
+    // const productData = JSON.parse(data);
+    res.writeHead(200, { 'Content-type': 'application/json' });
+    res.end(data);
+    // });
+  } else {
+    // ! status code must be sent before content
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world' // you can check this in 'Response Headers'
+    });
+    res.end('<h1>Page not found!</h1>');
+  }
 });
 
 // 8000: port number, 127.0.0.1 : local host address
