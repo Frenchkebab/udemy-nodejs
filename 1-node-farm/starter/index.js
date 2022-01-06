b47a343955679b5c631b03e5a475765c1b6edaa4
frenchkebab <tubowangin@gmail.com>
2022-01-06 12:36:22 +0900

    [#2] created first server with http module

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
+const http = require('http');
+/////////////////////////////////
+// ! FILES
 
-// //Blocking, synchronouse way
+////Blocking, synchronouse way
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 // console.log(textIn);
 
@@ -8,18 +11,29 @@ const fs = require('fs');
 // fs.writeFileSync('./txt/output.txt', textOut);
 // console.log('File written!');
 
-// Non-blocking, asynchronous way
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-  if (err) return console.log('ERROR!');
-  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-    console.log(data2);
-    fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
-      console.log(data3);
+//// Non-blocking, asynchronous way
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//   if (err) return console.log('ERROR!');
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     console.log(data2);
+//     fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+//       console.log(data3);
 
-      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
-        console.log('Your file has been written 😁');
-      });
-    });
-  });
-});
+//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
+//         console.log('Your file has been written 😁');
+//       });
+//     });
+//   });
+// });
 console.log('Will read file!');
+
+/////////////////////////////
+// SERVER
+const server = http.createServer((req, res) => {
+  res.end('Hello from the server!');
+});
+
+// 8000: port number, 127.0.0.1 : local host address
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000');
+});
