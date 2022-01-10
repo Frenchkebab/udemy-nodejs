5f871bea849590fda1ec789546c8f19f078db7ed
frenchkebab <tubowangin@gmail.com>
2022-01-10 15:28:17 +0900

    [#5] Building Promises

--- 3-asynchronous-JS/index.js
+++ 3-asynchronous-JS/index.js
@@ -1,19 +1,49 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  console.log(`Breed: ${data}`);
-
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`) //* HTTP get request
-    .then((res) => {
-      console.log(res.body.message);
-
-      fs.writeFile('dog-img.txt', res.body.message, (err) => {
-        console.log('Random dog image saved to file!');
-      });
-    })
-    .catch((err) => {
-      console.log(err.message);
+/*
+ * creating a new function which behind the scenes still runs
+ * the built in readFile function
+ * but then returns a promise so that we can use promise
+ * so that we can use the promise instead of the callback funciton
+ */
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('I could not find that file T.T');
+      /*
+       * Whatever variable that we pass into the resolve funciton
+       * will later be available as the argument in the then method
+       */
+      resolve(data); //* 'data' here is the value that this promise returns to us
     });
-});
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject('Could not write file T.T');
+      resolve('success');
+    });
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`) // if you put value in variable 'file' then this will return a Promise
+  .then((data) => {
+    console.log(`Breed: ${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`); // this code will return a Promise
+  }) //* HTTP get request
+  .then((res) => {
+    console.log(res.body.message);
+    return writeFilePro('dog-img.txt', res.body.message); // this code wil return a Promise
+    // fs.writeFile('dog-img.txt', res.body.message, (err) => {
+    //   console.log('Random dog image saved to file!');
+    // });
+  })
+  .then(() => {
+    console.log('Random dog image saved to file!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
