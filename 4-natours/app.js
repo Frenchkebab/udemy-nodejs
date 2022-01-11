fbce6a4775d81f13a0e21e63a23f4b1f294b7309
frenchkebab <tubowangin@gmail.com>
2022-01-11 10:33:52 +0900

    [#5] Handling GET requests

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,8 +1,10 @@
+const fs = require('fs');
 const express = require('express');
+const res = require('express/lib/response');
 
 const app = express();
 
-app.get('/', (req, res) => {
+/* app.get('/', (req, res) => {
   // res.status(200).send('Hello from the server side!');
 
   //* this response is only sent when a GET method (above) is sent to our server on this URL
@@ -15,7 +17,27 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
   res.send('You can post to this endpoint...');
+}); */
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+//* route handler
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours, // tours: tours
+    },
+  });
 });
+/*
+ * In case you want to do some changes to your API,
+ * you just can do it simply on v2
+ *  without breaking everyone who is still using V1
+ */
 
 const port = 3000;
 app.listen(port, () => {
