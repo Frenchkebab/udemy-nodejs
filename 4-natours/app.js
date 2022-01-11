2b6d96fd886cedb0c83c118d09d65fb81c61a9e6
frenchkebab <tubowangin@gmail.com>
2022-01-11 13:30:31 +0900

    [#5] Responding to URL Parameters - learned how to define parameters - and how to respond to it

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -4,6 +4,13 @@ const res = require('express/lib/response');
 
 const app = express();
 
+/*
+ * Middleware is a function that can modify the incomming request data.
+ * It is called 'Middle'ware because it stands between req and res
+ * It's a step that the request goes through while it's being processed.
+ * And as the req goes through, the data from the body is added to the req object using middleware.
+ */
+app.use(express.json()); //* Middleware
 /* app.get('/', (req, res) => {
   // res.status(200).send('Hello from the server side!');
 
@@ -20,7 +27,10 @@ app.post('/', (req, res) => {
 }); */
 
 const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`) //* only read at the beginning of the server
+  /*
+   * but each time we save the JSON file, it will also reload the server
+   */
 );
 
 //* route handler
@@ -33,6 +43,60 @@ app.get('/api/v1/tours', (req, res) => {
     },
   });
 });
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params); //* variables defined in URL
+
+  const id = req.params.id * 1; //* automatically converted into a number
+  const tour = tours.find((el) => el.id === req.params);
+
+  // if (id > tours.length) {
+  if (!tour) {
+    return res.status(404).json({
+      statis: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    // results: tours.length,
+    // data: {
+    //   tours, // tours: tours
+    // },
+    data: {
+      tour,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body); //* body will be available because we use Middleware
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  /*
+   * writeFile vs writeFileSync
+   * => we are inside of a callback function that is gonna run in the event loop
+   * so we can never ever block the event loop. So we should use async
+   */
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      //* 201: created
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
 /*
  * In case you want to do some changes to your API,
  * you just can do it simply on v2
